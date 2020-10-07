333c382426dbd37ce979a7674ed7eb8d1778be90
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2020-10-07 14:28:28 -0400

    Added some getters to simplify interface

--- pymint/mintterminal.py
+++ pymint/mintterminal.py
@@ -6,4 +6,8 @@ class MINTTerminal(MINTComponent):
     def __init__(self, name, port_number, layer:str = '0') -> None:
         super().__init__(name, 'TERMINAL', {}, layer)
         self.__port_number = port_number
+
+    @property
+    def port_number(self) -> int:
+        return self.__port_number
     
