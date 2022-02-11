9119a5f40855236c551e4fe77441e36bc92f79f3
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2022-02-11 17:20:29 -0500

    Changed the scope of DistanceDictionaries

--- pymint/constraints/mirrorconstraint.py
+++ pymint/constraints/mirrorconstraint.py
@@ -6,203 +6,200 @@ from pymint.mintcomponent import MINTComponent
 from pymint.mintdevice import MINTDevice
 
 
+class DistanceDictionaries:
+    """Distance dictionaries are the data structures we are using to solve the
+    mirror grouping problem. This algoithm creates dictionaries based on the
+    distances of each of the components to the `source components`that are the
+    original decendentants of the `mirror driving component`.
+
+    The datastructure essentially looks like this:
+    -----                                                                 -----
+    | 0 : [source_node]      0: [source_node]      0: [source_node]           |
+    | 1 : [node1, node2]     1: [node1, node2]     1: [node1, node2]          |
+    | 2 : [node1, node2]     2: [node1, node2]     2: [node1, node2]          |
+    | 3 : [node1, node2]  ,  3: [node1, node2]  ,  3: [node1, node2] ,  ...   |
+    | 4 : [node1, node2]     4: [node1, node2]     4: [node1, node2]          |
+    | 5 : [node1, node2]     5: [node1, node2]     5: [node1, node2]          |
+    | 6 : [node1, node2]     6: [node1, node2]     6: [node1, node2]          |
+    | 7 : [node1, node2]     7: [node1, node2]     7: [node1, node2]          |
+    -----                                                                 -----
+
+    Its a list of dictionaries where the keys are the distances to the source.
+    If we trim and prune this entire data structure, we will get the final
+    list of groups that we can export.
+
+    """
+
+    def __init__(
+        self, groups_size: int, device: MINTDevice, undirected_netlist
+    ) -> None:
+
+        # Initialize the dictionaries (the base data structure) with as many
+        # dictionaries as we have groups
+        self.dictionaries: List[Dict[int, List[str]]] = [{} for x in range(groups_size)]
+        self.netlist = undirected_netlist
+        self.device = device
+
+    def load_group_dfs_nodes(self, group_index, dfs_nodes: List[str]) -> None:
+        """
+        Loads the DFS nodes of a group into the dictionary.
+        """
+
+        # Find the source node of the group
+        source = dfs_nodes[0]
+        self.set_source(group_index, source)
+        # Find distances of each of the DFS nodes to the source node and save it into
+        # the dictionary
+        for node in dfs_nodes:
+            shortest_path = nx.shortest_path(self.netlist, source=source, target=node)
+            self.set_group_node(group_index, node, len(shortest_path))
+
+    def set_source(self, group_index: int, node: str):
+        self.dictionaries[group_index][0] = [node]
+
+    def set_group_node(self, group_index: int, node: str, distance: int):
+        if distance not in self.dictionaries[group_index]:
+            self.dictionaries[group_index][distance] = []
+        
+        self.dictionaries[group_index][distance].append(node)
+
+    def trim_uneven_distaces(self) -> None:
+        # Step 1 - Find the largest distance in each of the dictionaries
+        largest_distances = []
+        for group_dictionary in self.dictionaries:
+            largest_distances.append(max(group_dictionary.keys()))
+
+        # Step 2 - Find the min of these last distances and remove all the distances
+        # beyond that from the dictionaries
+        min_distance = min(largest_distances)
+
+        self.trim_dictionaries(min_distance + 1)
+
+    def trim_dictionaries(self, limit: int) -> None:
+        """Removes all the dictionarie entries that have a distance greater than or
+        equal to the limit"""
+        for group_dictionary in self.dictionaries:
+            for distance_key in group_dictionary.keys():
+                if distance_key >= limit:
+                    del group_dictionary[distance_key]
+
+    def prune_non_group_nodes(self) -> None:
+        # Step 1 - Go through each group
+        # Step 2 - Go through the nodes at each distance
+        # Step 3 - Get the shortest path from the source to each node
+        # Step 4 - If all the nodes in the shortest path are not in the group, remove
+        # the node from the group
+        for group_index in range(len(self.dictionaries)):
+            group_dictionary = self.dictionaries[group_index]
+            source = group_dictionary[0][0]
+            for distance in group_dictionary.keys():
+                nodes = group_dictionary[distance]
+                if(len(nodes) < 0):
+                    raise Exception("No nodes found at distance {}, group index {}".format(distance, group_index))
+                # Get shortest path
+                shortest_path = nx.shortest_path(
+                    self.netlist, source=source, target=nodes[0]
+                )
+                # If any of the noes in the shortest_path are not in the group, remove the node
+                for node in shortest_path:
+                    if self.is_node_in_group(node=node, group_index=group_index):
+                        self.remove_node_from_group(group_index, node)
+
+    def prune_non_matching_nodes(self) -> None:
+
+        # First check if the intersection of every level of types is null or not
+        # this will get rid of any levels that don't have the same types of nodes
+        ref_group = self.dictionaries[0]
+        found_flag = False
+        limit = 0
+        for distance_key in ref_group.keys():
+            ref_component_type_set = set(
+                [
+                    self.device.get_component(cid).entity
+                    for cid in ref_group[distance_key]
+                ]
+            )
+            for group_index in range(1, len(self.dictionaries)):
+                group_to_test = self.dictionaries[group_index]
+                type_set_to_test = set(
+                    [
+                        self.device.get_component(cid).entity
+                        for cid in group_to_test[distance_key]
+                    ]
+                )
+                if len(set.intersection(ref_component_type_set, type_set_to_test)) == 0:
+                    found_flag = True
+                    limit = distance_key
+                    break
+
+            if found_flag:
+                break
+
+        # If we find the mismatch, we delete the entire range starting from the limit
+        if found_flag:
+            self.trim_dictionaries(limit)
+
+        # Now we need to prune the excess components at each distance (This should get
+        # rid of excess cross links at distance = 1)
+
+        # Create a dictionary that keeps the list of the component types at each
+        # distance. This becomes the reference list that we pull for every group
+        # and pop elements from the copy to get the excess components
+        ref_group_types = {}
+        for distance_key in ref_group.keys():
+            nodes = ref_group[distance_key]
+            ref_group_types[distance_key] = [
+                self.device.get_component(cid).entity for cid in nodes
+            ]
+
+        for test_group_index in range(1, len(self.dictionaries)):
+            test_group = self.dictionaries[test_group_index]
+
+            for distance_key in test_group.keys():
+                ref_types_list = ref_group_types[distance_key].copy()
+                test_nodes = test_group[distance_key]
+                # Loop through the test nodes and remove their corresponding types from the ref_types_list
+                for node_index in range(len(test_nodes)):
+                    test_node = test_nodes[node_index]
+                    ref_types_list.remove(self.device.get_component(test_node).entity)
+                    if len(ref_types_list) == 0:
+                        # Remove all the remaining nodes and break from the loop
+                        for node_to_remove_index in range(
+                            node_index + 1, len(test_nodes)
+                        ):
+                            self.remove_node_from_group(
+                                test_group_index, test_nodes[node_to_remove_index]
+                            )
+                        break
+
+    def is_node_in_group(self, group_index: int, node: str) -> bool:
+        group_dictionary = self.dictionaries[group_index]
+        for nodes in group_dictionary.values():
+            if node in nodes:
+                return True
+        return False
+
+    def remove_node_from_group(self, group_index: int, node: str) -> None:
+        group_dictionary = self.dictionaries[group_index]
+        for nodes in group_dictionary.values():
+            if node in nodes:
+                nodes.remove(node)
+
+    def generate_groups(self) -> List[List[str]]:
+        groups = []
+        for group_index in range(len(self.dictionaries)):
+            group_dictionary = self.dictionaries[group_index]
+            nodes = []
+            for distance in group_dictionary.keys():
+                nodes.extend(group_dictionary[distance])
+            groups.append(nodes)
+        return groups
+
+
 class MirrorConstraint(LayoutConstraint):
     """Layout constraint when two differnt sub-netlists need to have
     the same layout
     """
-
-    class DistanceDictionaries:
-        """Distance dictionaries are the data structures we are using to solve the
-        mirror grouping problem. This algoithm creates dictionaries based on the
-        distances of each of the components to the `source components`that are the 
-        original decendentants of the `mirror driving component`.
-
-        The datastructure essentially looks like this:
-        -----                                                                 -----
-        | 0 : [source_node]      0: [source_node]      0: [source_node]           |
-        | 1 : [node1, node2]     1: [node1, node2]     1: [node1, node2]          |
-        | 2 : [node1, node2]     2: [node1, node2]     2: [node1, node2]          |
-        | 3 : [node1, node2]  ,  3: [node1, node2]  ,  3: [node1, node2] ,  ...   |
-        | 4 : [node1, node2]     4: [node1, node2]     4: [node1, node2]          |
-        | 5 : [node1, node2]     5: [node1, node2]     5: [node1, node2]          |
-        | 6 : [node1, node2]     6: [node1, node2]     6: [node1, node2]          |
-        | 7 : [node1, node2]     7: [node1, node2]     7: [node1, node2]          |
-        -----                                                                 -----
-
-        Its a list of dictionaries where the keys are the distances to the source.
-        If we trim and prune this entire data structure, we will get the final 
-        list of groups that we can export.
-
-        """
-
-        def __init__(
-            self, groups_size: int, device: MINTDevice, undirected_netlist
-        ) -> None:
-
-            # Initialize the dictionaries (the base data structure) with as many
-            # dictionaries as we have groups
-            self.dictionaries: List[Dict[int, List[str]]] = [
-                {} for x in range(groups_size)
-            ]
-            self.netlist = undirected_netlist
-            self.device = device
-
-        def load_group_dfs_nodes(self, group_index, dfs_nodes: List[str]) -> None:
-            """
-            Loads the DFS nodes of a group into the dictionary.
-            """
-
-            # Find the source node of the group
-            source = dfs_nodes[0]
-            self.set_source(group_index, source)
-            # Find distances of each of the DFS nodes to the source node and save it into
-            # the dictionary
-            for node in dfs_nodes:
-                shortest_path = nx.shortest_path(
-                    self.netlist, source=source, target=node
-                )
-                self.set_group_node(group_index, node, len(shortest_path))
-
-        def set_source(self, group_index: int, node: str):
-            self.dictionaries[group_index][0].append(node)
-
-        def set_group_node(self, group_index: int, node: str, distance: int):
-            self.dictionaries[group_index][distance].append(node)
-
-        def trim_uneven_distaces(self) -> None:
-            # Step 1 - Find the largest distance in each of the dictionaries
-            largest_distances = []
-            for group_dictionary in self.dictionaries:
-                largest_distances.append(max(group_dictionary.keys()))
-
-            # Step 2 - Find the min of these last distances and remove all the distances
-            # beyond that from the dictionaries
-            min_distance = min(largest_distances)
-
-            self.trim_dictionaries(min_distance + 1)
-
-        def trim_dictionaries(self, limit: int) -> None:
-            """Removes all the dictionarie entries that have a distance greater than or
-            equal to the limit"""
-            for group_dictionary in self.dictionaries:
-                for distance_key in group_dictionary.keys():
-                    if distance_key >= limit:
-                        del group_dictionary[distance_key]
-
-        def prune_non_group_nodes(self) -> None:
-            # Step 1 - Go through each group
-            # Step 2 - Go through the nodes at each distance
-            # Step 3 - Get the shortest path from the source to each node
-            # Step 4 - If all the nodes in the shortest path are not in the group, remove
-            # the node from the group
-            for group_index in range(len(self.dictionaries)):
-                group_dictionary = self.dictionaries[group_index]
-                source = group_dictionary[0][0]
-                for distance in group_dictionary.keys():
-                    nodes = group_dictionary[distance]
-                    # Get shortest path
-                    shortest_path = nx.shortest_path(
-                        self.netlist, source=source, target=nodes[0]
-                    )
-                    # If any of the noes in the shortest_path are not in the group, remove the node
-                    for node in shortest_path:
-                        if self.is_node_in_group(node=node, group_index=group_index):
-                            self.remove_node_from_group(group_index, node)
-
-        def prune_non_matching_nodes(self) -> None:
-
-            # First check if the intersection of every level of types is null or not
-            # this will get rid of any levels that don't have the same types of nodes
-            ref_group = self.dictionaries[0]
-            found_flag = False
-            limit = 0
-            for distance_key in ref_group.keys():
-                ref_component_type_set = set(
-                    [
-                        self.device.get_component(cid).entity
-                        for cid in ref_group[distance_key]
-                    ]
-                )
-                for group_index in range(1, len(self.dictionaries)):
-                    group_to_test = self.dictionaries[group_index]
-                    type_set_to_test = set(
-                        [
-                            self.device.get_component(cid).entity
-                            for cid in group_to_test[distance_key]
-                        ]
-                    )
-                    if (
-                        len(set.intersection(ref_component_type_set, type_set_to_test))
-                        == 0
-                    ):
-                        found_flag = True
-                        limit = distance_key
-                        break
-
-                if found_flag:
-                    break
-
-            # If we find the mismatch, we delete the entire range starting from the limit
-            if found_flag:
-                self.trim_dictionaries(limit)
-
-            # Now we need to prune the excess components at each distance (This should get
-            # rid of excess cross links at distance = 1)
-
-            # Create a dictionary that keeps the list of the component types at each
-            # distance. This becomes the reference list that we pull for every group
-            # and pop elements from the copy to get the excess components
-            ref_group_types = {}
-            for distance_key in ref_group.keys():
-                nodes = ref_group[distance_key]
-                ref_group_types[distance_key] = [
-                    self.device.get_component(cid).entity for cid in nodes
-                ]
-
-            for test_group_index in range(1, len(self.dictionaries)):
-                test_group = self.dictionaries[test_group_index]
-
-                for distance_key in test_group.keys():
-                    ref_types_list = ref_group_types[distance_key].copy()
-                    test_nodes = test_group[distance_key]
-                    # Loop through the test nodes and remove their corresponding types from the ref_types_list
-                    for node_index in range(len(test_nodes)):
-                        test_node = test_nodes[node_index]
-                        ref_types_list.remove(
-                            self.device.get_component(test_node).entity
-                        )
-                        if len(ref_types_list) == 0:
-                            # Remove all the remaining nodes and break from the loop
-                            for node_to_remove_index in range(
-                                node_index + 1, len(test_nodes)
-                            ):
-                                self.remove_node_from_group(
-                                    test_group_index, test_nodes[node_to_remove_index]
-                                )
-                            break
-
-        def is_node_in_group(self, group_index: int, node: str) -> bool:
-            group_dictionary = self.dictionaries[group_index]
-            for nodes in group_dictionary.values():
-                if node in nodes:
-                    return True
-            return False
-
-        def remove_node_from_group(self, group_index: int, node: str) -> None:
-            group_dictionary = self.dictionaries[group_index]
-            for nodes in group_dictionary.values():
-                if node in nodes:
-                    nodes.remove(node)
-
-        def generate_groups(self) -> List[List[str]]:
-            groups = []
-            for group_index in range(len(self.dictionaries)):
-                group_dictionary = self.dictionaries[group_index]
-                nodes = []
-                for distance in group_dictionary.keys():
-                    nodes.extend(group_dictionary[distance])
-                groups.append(nodes)
-            return groups
 
     def __init__(
         self,
